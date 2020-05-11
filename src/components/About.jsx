1e1acd7045749183db780c0b08e0aab72b14e34d
Lowella Fox <53575035+Lowella369@users.noreply.github.com>
2020-05-11 14:27:06 -0500

    Creating About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      This is a simple to do list web application that allow the user to...
+    </div>
+  );
+};
+
+export default About;
