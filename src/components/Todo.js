23e394905e75d11ad0732511b22b40fd52a9ba77
Lowella Fox <53575035+Lowella369@users.noreply.github.com>
2020-05-11 14:35:58 -0500

    adding action creator

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,3 +9,46 @@ const SHOW_ALL = "SHOW_ALL";
 const SHOW_ACTIVE = "SHOW_ACTIVE";
 const SHOW_COMPLETED = "SHOW_COMPLETED";
 const DELETE_TODO = "DELETE_TODO";
+
+//action creator
+const addTodo = value => {
+  return {
+    type: ADD_TODO,
+    text: value,
+    isCheck: false,
+    Id: nextTodoId++,
+    isVisible: true
+  };
+};
+
+const toggleTodo = id => {
+  return {
+    type: TOGGLE_TODO,
+    Id: id
+  };
+};
+
+const showAll = () => {
+  return {
+    type: SHOW_ALL
+  };
+};
+
+const showActive = () => {
+  return {
+    type: SHOW_ACTIVE
+  };
+};
+
+const showCompleted = () => {
+  return {
+    type: SHOW_COMPLETED
+  };
+};
+
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    Id: id
+  };
+};
