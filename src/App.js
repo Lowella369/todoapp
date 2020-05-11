08f874dff77a837e909441e974ab0e10988d4b41
Lowella Fox <53575035+Lowella369@users.noreply.github.com>
2020-05-11 14:34:18 -0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+
 import "./styles.css";
 
-export default function App() {
+import NavBar from "./components/NavBar.jsx";
+
+import About from "./components/About.jsx";
+import Error from "./components/Error.jsx";
+import Todo from "./components/Todo.js";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Redirect exact path="/" to="/about" />
+          <Route path="/about" component={About} />
+          <Route path="/todos" component={Todo} />
+          <Route path="/contact" component={null} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;
