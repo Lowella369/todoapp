a9719a07f0f3540f7acd96e5c10d2a65f1ee6efa
Lowella Fox <53575035+Lowella369@users.noreply.github.com>
2020-05-11 14:25:19 -0500

    creating NavTab

---
+++ src/components/NavTab.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+
+const NavTab = props => {
+  var activeStyle = {
+    color: "#bafae7",
+    fontWeight: "bold"
+  };
+
+  var navStyle = {
+    margin: "2rem",
+    textDecoration: "none",
+    fontSize: "1rem",
+    color: "#f6fbfa",
+    display: "inline"
+  };
+
+  return (
+    <NavLink style={navStyle} activeStyle={activeStyle} to={props.to}>
+      {props.label}
+    </NavLink>
+  );
+};
+
+export default NavTab;
