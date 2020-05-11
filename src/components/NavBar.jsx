40a8173a98e3a547cb3e721ee25e900502a58353
Lowella Fox <53575035+Lowella369@users.noreply.github.com>
2020-05-11 14:30:26 -0500

    creating NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavTab from "./NavTab.jsx";
+
+import "./css/style.css";
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <NavTab to="/about" label="About" />
+      <NavTab to="/todos" label="Todos" />
+      <NavTab to="/contact" label="Contact" />
+    </div>
+  );
+};
+
+export default NavBar;
