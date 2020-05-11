45ae13e66636c43996b701b086628d02bf22cd3f
Lowella Fox <53575035+Lowella369@users.noreply.github.com>
2020-05-11 14:41:49 -0500

    creating presentational component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -52,3 +52,181 @@ const deleteTodo = id => {
     Id: id
   };
 };
+
+//presentational components
+let nextTodoId = 0;
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleChange = e => {
+    const value = e.target.value;
+    this.setState({
+      value
+    });
+  };
+
+  handleKeyUp = (value, e) => {
+    if (e.key === "Enter") {
+      //this.state.value.trim() -> this is the current input value
+      this.state.value.trim().length && this.props.dispatch(addTodo(value));
+
+      this.setState({
+        value: ""
+      });
+    }
+  };
+
+  handleSubmit = value => {
+    this.state.value.trim().length && this.props.dispatch(addTodo(value));
+
+    this.setState({
+      value: ""
+    });
+  };
+
+  handleClearIcon = () => {
+    this.setState({
+      value: ""
+    });
+  };
+
+  handleCheckboxClick = id => {
+    this.props.dispatch(toggleTodo(id));
+  };
+
+  showActive = () => {
+    this.props.dispatch(showActive());
+  };
+
+  showCompleted = () => {
+    this.props.dispatch(showCompleted());
+  };
+
+  showAll = () => {
+    this.props.dispatch(showAll());
+  };
+
+  handleDeletClick = id => {
+    this.props.dispatch(deleteTodo(id));
+  };
+
+  render() {
+    const value = this.state.value;
+    const todos = this.props.todos;
+    const visibleElements = todos.filter(item => item.isVisible);
+    return (
+      <React.Fragment>
+        <h1>Todo App</h1>
+        <div className="todoApp">
+          <div className="todoContainer">
+            <div className="inputFields">
+              <input
+                className="inputs"
+                type="text"
+                value={value}
+                onChange={this.handleChange}
+                onKeyUp={this.handleKeyUp.bind(this, value)}
+              />
+              <span
+                className={`input-clear-icon ${
+                  value !== "" ? "clear-icon--show" : ""
+                } `}
+                onClick={this.handleClearIcon}
+              >
+                {" "}
+                X
+              </span>
+            </div>
+            <button
+              className="btn"
+              type="button"
+              onClick={this.handleSubmit.bind(this, value)}
+            >
+              Submit
+            </button>
+          </div>
+          {visibleElements.length > 0 && (
+            <ul className="list">
+              {todos.map((item, index) => {
+                return (
+                  item.isVisible && (
+                    <li className="listItem" key={item.Id}>
+                      <label
+                        className={`list-label ${
+                          item.isCheck ? "line-through" : ""
+                        }`}
+                      >
+                        <input
+                          defaultChecked={item.isCheck}
+                          type="checkbox"
+                          onClick={this.handleCheckboxClick.bind(this, item.Id)}
+                        />
+                        {item.text}
+                      </label>
+                      <span
+                        className="delete-btn"
+                        role="button"
+                        onClick={this.handleDeletClick.bind(this, item.Id)}
+                      >
+                        Delete
+                      </span>
+                    </li>
+                  )
+                );
+              })}
+            </ul>
+          )}
+          {todos.length === 0 ? (
+            <p>Add a TODO List</p>
+          ) : visibleElements.length === 0 ? (
+            <p>This Filter is empty</p>
+          ) : (
+            ""
+          )}
+
+          <div className="filterContainer">
+            Show:
+            <input
+              type="radio"
+              name="filter"
+              id="allFilter"
+              className="filter-input"
+              onClick={this.showAll}
+              disabled={todos.length === 0}
+            />
+            <label htmlFor="allFilter" className="filterLabel">
+              All
+            </label>
+            <input
+              type="radio"
+              name="filter"
+              id="activeFilter"
+              className="filter-input"
+              onClick={this.showActive}
+              disabled={todos.length === 0}
+            />
+            <label htmlFor="activeFilter" className="filterLabel">
+              Active
+            </label>
+            <input
+              type="radio"
+              name="filter"
+              id="completedFilter"
+              className="filter-input"
+              onClick={this.showCompleted}
+              disabled={todos.length === 0}
+            />
+            <label htmlFor="completedFilter" className="filterLabel">
+              Completed
+            </label>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
