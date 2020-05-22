e2adeae6be4d8375087cf8dcd89c42df079aad2d
Lowella369 <lh850210@gmail.com>
2020-05-22 11:24:07 -0500

    Updates

---
+++ precache-manifest.6492142dec54a877a86792c7b1810eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067f9846b9ca6905d53d99a81b54b684",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "5c1f723aa612d44f377b",
+    "url": "/todoapp/static/css/main.c9ed2b76.chunk.css"
+  },
+  {
+    "revision": "fefd56c7f778ed595054",
+    "url": "/todoapp/static/js/2.3c5f9a6e.chunk.js"
+  },
+  {
+    "revision": "5c1f723aa612d44f377b",
+    "url": "/todoapp/static/js/main.5771b22d.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
